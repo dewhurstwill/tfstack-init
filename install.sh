6314c7a0b522ef77d9daa9fa5634f23802665d51
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-29 14:44:51 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-curl -fsSL -o ~/tfstack-init/init.sh https://raw.githubusercontent.com/dewhurstwill/tfstack-init/main/tfstack-init.sh
+curl -o ~/tfstack-init.sh https://raw.githubusercontent.com/dewhurstwill/tfstack-init/main/tfstack-init.sh
 echo alias tfstack-init='/bin/bash -c "~/tfstack-init/init.sh"' >> ~/.bash_profile
 echo alias tfstack-init='/bin/bash -c "~/tfstack-init/init.sh"' >> ~/.bash_rc
