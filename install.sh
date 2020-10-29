345985300f0757d2e6b04684a3f4370330d0a6f3
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2020-10-29 14:34:02 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+echo alias tfstack-init='/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/dewhurstwill/tfstack-init/main/tfstack-init.sh)"' >> ~/.bash_profile
+echo alias tfstack-init='/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/dewhurstwill/tfstack-init/main/tfstack-init.sh)"' >> ~/.bash_rc
